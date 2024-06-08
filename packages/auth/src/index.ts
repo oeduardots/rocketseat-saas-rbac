d1e2fe47f0ef86d32915ca6e14133e5b33001feb
Eduardo Tavares <edutav.dev@gmail.com>
2024-06-08 15:46:37 -0300

    definindo permissoes do app

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -8,18 +8,22 @@ import { z } from 'zod'
 
 import { User } from './models/user'
 import { permissions } from './permissions'
-import { bilingSubject } from './subjects/biling'
+import { billingSubject } from './subjects/billing'
 import { inviteSubject } from './subjects/invite'
 import { organizationSubject } from './subjects/organization'
 import { projectSubject } from './subjects/project'
 import { userSubject } from './subjects/user'
+
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
 
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,
   organizationSubject,
   inviteSubject,
-  bilingSubject,
+  billingSubject,
 
   z.tuple([z.literal('manage'), z.literal('all')]),
 ])
