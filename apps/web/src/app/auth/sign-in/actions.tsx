fa5fb6ef70d8c403ac6bb2286362111a02d75412
Eduardo Tavares <edutav.dev@gmail.com>
2024-06-14 01:51:52 -0300

    setup do cliente http ky

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,3 +1,14 @@
 'use server'
 
-export async function signInWithEmailAndPassword(data: FormData) {}
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+export async function signInWithEmailAndPassword(data: FormData) {
+  const { email, password } = Object.fromEntries(data)
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  console.log(result)
+}
